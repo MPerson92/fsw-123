2c54a3fe54daaabe329e433be5dd14364f531ba9
MPerson92 <76800220+MPerson92@users.noreply.github.com>
2021-06-30 20:07:10 -0400

    updatedtodo

--- week3/store/src/Store.js
+++ week3/store/src/Store.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import Todo from './Todo'
-
-export default function Store({ todos, pickTodo }) {
-    return (
-       todos.map(todo => {
-           return <Todo key={todo.id} pickTodo = {pickTodo} 
-           todo = {todo}  />
-       })
-    )
-}
+import { v4 as uuidv4 } from "uuid";
 
 
+export const listOfTodos = [
+    {
+        "id": uuidv4(),
+        "text": "wash clothes",
+        "isCompleted": true
+    },
+    {
+        "id": uuidv4(),
+        "text": "shop for shoes",
+        "isCompleted": false
+    },
+    {
+        "id": uuidv4(),
+        "text": "walk a mile",
+        "isCompleted": true
+    },
+    {
+        "id": uuidv4(),
+        "text": "cleaning the house",
+        "isCompleted": false
+    }
+    ];
 
+
+export default listOfTodos;
